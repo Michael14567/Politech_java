2ac30df8107dcf88aa14f11b65dc112342137a2d
Dnec <78984255+Michael14567@users.noreply.github.com>
2024-12-02 04:02:05 +0300

    Генерация документации

---
+++ Example.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * @author Dnec
+ * @version 1.0
+ * @since 2024-12-02
+ */
+public class Example {
+
+    /**
+     * Метод вычисляет сумму двух чисел.
+     *
+     * @param a первое число
+     * @param b второе число
+     * @return сумма чисел a и b
+     */
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Главный метод программы.
+     *
+     * @param args аргументы командной строки
+     */
+    public static void main(String[] args) {
+        Example example = new Example();
+        int result = example.sum(5, 10);
+        System.out.println("Сумма: " + result);
+    }
+}
---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Example","l":"Example()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Example","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Example","l":"sum(int, int)","u":"sum(int,int)"}];updateSearchResults();
