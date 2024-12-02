2fe8c25687b84cf6b33422da3567594501c8a9bd
Dnec <78984255+Michael14567@users.noreply.github.com>
2024-12-02 04:25:30 +0300

    Оператор условного перехода

---
+++ Conditional_jump_operator.java
@@ -0,0 +1,8 @@
+public class Conditional_jump_operator {
+    public static void main(String[] args) {
+        int a = 2; // Попробуйте изменить значение a
+        int i = (a == 1) ? 1 : (a == 2) ? 2 : 3;
+
+        System.out.println("Значение i: " + i);
+    }
+}
